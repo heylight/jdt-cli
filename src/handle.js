36dcf481f5485bc0e11451ea33d31e61367a9013
zhuhailiang4 <zhuhailiang@jd.com>
2021-02-18 15:45:04 +0800

    1

--- src/handle.js
+++ src/handle.js
@@ -6,30 +6,31 @@ import { exec } from "child_process";
 import { compile } from "handlebars";
 
 async function copyConfig(to, answers) {
-  let pkg = require(`../template/${answers.template}/package.json`);
+  let pkg = `../template/${answers.template}/package.json`;
   await write(path.join(to, "package.json"), compile(pkg)(answers), {
     overwrite: true,
   });
 
   if (answers.template === "cms") {
-    let env = require(`../template/${answers.template}/.env`);
+    let env = `../template/${answers.template}/.env`;
     await write(path.join(to, ".env"), compile(env)(answers), {
       overwrite: true,
     });
   }
-  console.log(answers);
-  // if (answers.install) {
-  //   let i = 0;
-  //   let spin = setInterval(() => {
-  //     const { frames } = cliSpinners.dots;
-  //     process.stdout.write(`\r${frames[(i = ++i % frames.length)]} install...`);
-  //   }, cliSpinners.dots.interval);
-  //   exec(`cd ${answers.name}&&npm i`, function (err) {
-  //     clearInterval(spin);
-  //     process.stdout.clearLine();
-  //     console.log("\n完成!\n");
-  //   });
-  // }
+  if (answers.install) {
+    let i = 0;
+    let spin = setInterval(() => {
+      const { frames } = cliSpinners.dots;
+      process.stdout.write(`\r${frames[(i = ++i % frames.length)]} install...`);
+    }, cliSpinners.dots.interval);
+    exec(`cd ${answers.name}&&npm i`, function (err) {
+      clearInterval(spin);
+      process.stdout.clearLine();
+      console.log("\n完成!\n");
+    });
+  } else {
+    console.log("\n完成!\n");
+  }
 }
 function handle(answers) {
   const from = path.resolve(__dirname, `../template/${answers.template}/**/*`);
